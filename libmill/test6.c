1e4482a22d234dd54e8fd04c89afd1422596c594
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-03-18 19:40:28 +0100

    Channels basic usage

---
+++ libmill/test6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "libmill.h"
+
+coroutine void sender(chan channel) {
+    msleep(now() + 1000);
+    chs(channel, int, '*');
+    chclose(channel);
+}
+
+int main() {
+    chan channel = chmake(int, 0);
+    puts("vytvoreni korutiny");
+    go(sender(chdup(channel)));
+    puts("cekani na zpravu z korutiny");
+    char c = chr(channel, int);
+    puts("prijato");
+    putchar(c);
+    chclose(channel);
+    return 0;
+}
