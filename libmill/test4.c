6386395d506b30cfd50acf67b0e3e00b0cc594f6
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-03-18 15:51:40 +0100

    Wait/sleep in main coroutine

---
+++ libmill/test4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "libmill.h"
+
+coroutine void print(char what) {
+    int i;
+
+    for (i=0; i<10; i++) {
+        msleep(now() + 100);
+        putchar(what);
+    }
+}
+
+int main(void) {
+    char ch;
+    for (ch='a'; ch<='z'; ch++) {
+        go(print(ch));
+    }
+    msleep(now() + 200);
+    return 0;
+}
