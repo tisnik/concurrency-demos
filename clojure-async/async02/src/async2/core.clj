6d83738838093e8a506007de8598d33fe4883bd2
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-24 08:45:48 +0200

    Reformatted core.clj by zprint

--- clojure-async/async02/src/async2/core.clj
+++ clojure-async/async02/src/async2/core.clj
@@ -11,7 +11,7 @@
 ;
 
 (ns async2.core
-    (:gen-class))
+  (:gen-class))
 
 ; nacteme vsechny potrebne funkce, makra a symboly z knihovny
 ; (schvalne se nenacitaji vsechny funkce, protoze by jejich jmena
@@ -19,32 +19,32 @@
 (require '[clojure.core.async :refer (go chan >! <! timeout)])
 
 (defn wait
-    "Pozastaveni hlavniho vlakna - simulace interaktivni prace."
-    []
-    (Thread/sleep 15000))
+  "Pozastaveni hlavniho vlakna - simulace interaktivni prace."
+  []
+  (Thread/sleep 15000))
 
 (defn -main
-    "Tato funkce se spusti automaticky nastrojem Leiningen."
-    [& args]
-    (println "Start")
-    ; vytvorime kanal
-    (let [channel (chan)]
-        ; kontinualni posilani zprav do kanalu v asynchronnim bloku
-        (go
-            (dotimes [i 10]
-                ;(Thread/sleep 1000)
-                (<! (timeout 1000))
-                (>! channel i)))
+  "Tato funkce se spusti automaticky nastrojem Leiningen."
+  [& args]
+  (println "Start")
+  ; vytvorime kanal
+  (let [channel (chan)]
+    ; kontinualni posilani zprav do kanalu v asynchronnim bloku
+    (go
+        (dotimes [i 10]
+            ;(Thread/sleep 1000)
+            (<! (timeout 1000))
+            (>! channel i)))
 
-        (println "1st go block started")
+    (println "1st go block started")
 
-        ; kontinualni cteni zprav z kanalu v asynchronnim bloku
-        (go
-            (while true
-                (println (<! channel))))
+    ; kontinualni cteni zprav z kanalu v asynchronnim bloku
+    (go
+        (while true
+            (println (<! channel))))
 
-        (println "2nd go block started"))
-    ; chvili pockame, az se vypise cela sekvence 0 az 9
-    (wait)
-    (println "Finish"))
+    (println "2nd go block started"))
+  ; chvili pockame, az se vypise cela sekvence 0 az 9
+  (wait)
+  (println "Finish"))
 
