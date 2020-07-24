a4848dd7fc1e01ec3d048d73346dda084a29d9ba
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-24 08:45:48 +0200

    Reformatted core.clj by zprint

--- clojure-async/async01/src/async1/core.clj
+++ clojure-async/async01/src/async1/core.clj
@@ -11,7 +11,7 @@
 ;
 
 (ns async1.core
-    (:gen-class))
+  (:gen-class))
 
 ; nacteme vsechny potrebne funkce, makra a symboly z knihovny
 ; (schvalne se nenacitaji vsechny funkce, protoze by jejich jmena
@@ -19,31 +19,32 @@
 (require '[clojure.core.async :refer (go chan >! <!)])
 
 (defn wait
-    "Pozastaveni hlavniho vlakna - simulace interaktivni prace."
-    []
-    (Thread/sleep 1000))
+  "Pozastaveni hlavniho vlakna - simulace interaktivni prace."
+  []
+  (Thread/sleep 1000))
 
 (defn -main
-    "Tato funkce se spusti automaticky nastrojem Leiningen."
-    [& args]
-    (println "Start")
-    ; vytvorime kanal
-    (let [channel (chan)]
-        ; poslani zpravy do kanaly (go block bude cekat na precteni)
-        (go (>! channel "Hello world #1!"))
-        (wait)
+  "Tato funkce se spusti automaticky nastrojem Leiningen."
+  [& args]
+  (println "Start")
+  ; vytvorime kanal
+  (let [channel (chan)]
+    ; poslani zpravy do kanaly (go block bude cekat na precteni)
+    (go (>! channel "Hello world #1!"))
+    (wait)
 
-        ; precteni zpravy z kanalu
-        (go (println (<! channel)))
-        (wait)
-        
-        ; pokus o precteni zpravy z kanalu (ten je prazdny, takze se go block zastavi)
-        (go (println (<! channel)))
-        (wait)
+    ; precteni zpravy z kanalu
+    (go (println (<! channel)))
+    (wait)
 
-        ; poslani zpravy do kanalu, na tuto zpravu se jiz netrpelive ceka
-        (go (>! channel "Hello world #2!"))
-        (wait))
+    ; pokus o precteni zpravy z kanalu (ten je prazdny, takze se go block
+    ; zastavi)
+    (go (println (<! channel)))
+    (wait)
 
-    (println "Finish"))
+    ; poslani zpravy do kanalu, na tuto zpravu se jiz netrpelive ceka
+    (go (>! channel "Hello world #2!"))
+    (wait))
+
+  (println "Finish"))
 
